d4ac4c99799354e9b8afe8395caf798b56b387f5
William <william@lexing360.com>
2018-02-08 14:48:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/unclewei/accessbilityapplication/service/AccessibilityService.java
@@ -0,0 +1,21 @@
+package com.example.unclewei.accessbilityapplication.service;
+
+
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * @author unclewei
+ * @Data 2018/2/8.
+ */
+
+public class AccessibilityService extends android.accessibilityservice.AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
